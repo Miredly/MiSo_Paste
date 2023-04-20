536f3b2143c869863113903fc33d89a758e7a652
Miredly <miles@mired.space>
2023-04-19 16:26:38 -1000

    Redo everything in EGUI JUST BECAUSE

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub use crate::eq::EQSTATE;
 mod tapeloop;
 pub use crate::tapeloop::TAPESTATE;
 use nih_plug::prelude::*;
+use nih_plug_egui::{create_egui_editor, egui, widgets, EguiState};
 use std::sync::Arc;
 
 // This is a shortened version of the gain example with most comments removed, check out
@@ -47,6 +48,9 @@ struct MisoFirstParams {
     pub clear: BoolParam,
     #[id = "erase"]
     pub erase: BoolParam,
+
+    #[persist = "editor-state"]
+    editor_state: Arc<EguiState>,
 }
 
 impl Default for MisoFirstParams {
@@ -120,6 +124,8 @@ impl Default for MisoFirstParams {
             clear: BoolParam::new("clear", false),
 
             erase: BoolParam::new("erase", false),
+
+            editor_state: EguiState::from_size(512, 512),
         }
     }
 }
@@ -163,6 +169,69 @@ impl Plugin for MisoFirst {
 
     fn params(&self) -> Arc<dyn Params> {
         self.params.clone()
+    }
+
+    fn editor(&self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        let params = self.params.clone();
+        // let peak_meter = self.peak_meter.clone();
+        create_egui_editor(
+            self.params.editor_state.clone(),
+            (),
+            |_, _| {},
+            move |egui_ctx, setter, _state| {
+                egui::CentralPanel::default().show(egui_ctx, |ui| {
+                    // NOTE: See `plugins/diopser/src/editor.rs` for an example using the generic UI widget
+
+                    // This is a fancy widget that can get all the information it needs to properly
+                    // display and modify the parameter from the parametr itself
+                    // It's not yet fully implemented, as the text is missing.
+                    // ui.label("Some random integer");
+                    // ui.add(widgets::ParamSlider::for_param(&params.some_int, setter));
+
+                    ui.label("Gain");
+                    ui.add(widgets::ParamSlider::for_param(&params.gain, setter));
+
+                    ui.label(
+                        "Also gain, but with a lame widget. Can't even render the value correctly!",
+                    );
+                    // This is a simple naieve version of a parameter slider that's not aware of how
+                    // the parameters work
+                    ui.add(
+                        egui::widgets::Slider::from_get_set(-30.0..=30.0, |new_value| {
+                            match new_value {
+                                Some(new_value_db) => {
+                                    let new_value = util::gain_to_db(new_value_db as f32);
+
+                                    setter.begin_set_parameter(&params.gain);
+                                    setter.set_parameter(&params.gain, new_value);
+                                    setter.end_set_parameter(&params.gain);
+
+                                    new_value_db
+                                }
+                                None => util::gain_to_db(params.gain.value()) as f64,
+                            }
+                        })
+                        .suffix(" dB"),
+                    );
+
+                    // TODO: Add a proper custom widget instead of reusing a progress bar
+                    // let peak_meter =
+                    //     util::gain_to_db(peak_meter.load(std::sync::atomic::Ordering::Relaxed));
+                    // let peak_meter_text = if peak_meter > util::MINUS_INFINITY_DB {
+                    //     format!("{peak_meter:.1} dBFS")
+                    // } else {
+                    //     String::from("-inf dBFS")
+                    // };
+
+                    // let peak_meter_normalized = (peak_meter + 60.0) / 60.0;
+                    // ui.allocate_space(egui::Vec2::splat(2.0));
+                    // ui.add(
+                    //     egui::widgets::ProgressBar::new(peak_meter_normalized)
+                    //         .text(peak_meter_text),
+                    // );
+                });
+            },
+        )
     }
 
     fn initialize(
