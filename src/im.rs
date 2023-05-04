6cb559caeb4f533b78a38813438e326658968fa3
Miredly <miles@mired.space>
2023-05-03 14:22:28 -1000

    trim orphaned code

--- src/im.rs
+++ src/im.rs
@@ -22,25 +22,11 @@ impl Default for UiImages {
     }
 }
 
-pub fn load_image_from_path(path: &str) -> Result<ColorImage, image::ImageError> {
-    nih_dbg!(path);
-    nih_dbg!(std::env::current_exe().unwrap());
-    let image = image::io::Reader::open(get_path(path))?.decode()?;
-    nih_dbg!("at least the path is good");
-    let size = [image.width() as _, image.height() as _];
-    let image_buffer = image.to_rgba8();
-    let pixels = image_buffer.as_flat_samples();
-    Ok(ColorImage::from_rgba_unmultiplied(size, pixels.as_slice()))
-}
-
 pub fn load_image_from_memory(img: &[u8]) -> Result<ColorImage, image::ImageError> {
     let image = image::load_from_memory(img).expect("couldn't load");
     let size = [image.width() as _, image.height() as _];
     let image_buffer = image.to_rgba8();
     let pixels = image_buffer.as_flat_samples();
-    Ok(ColorImage::from_rgba_unmultiplied(size, pixels.as_slice()))
-}
 
-fn get_path(path: &str) -> &std::path::Path {
-    return std::path::Path::new(path);
+    Ok(ColorImage::from_rgba_unmultiplied(size, pixels.as_slice()))
 }
